9683764dec33c80dcf0350c222b2df1a0a95b0c3
flowonthego <78504760+flowonthego@users.noreply.github.com>
2021-04-30 15:53:34 +0100

    Update and rename hello.py to create_data.py

---
+++ create_data.py
@@ -0,0 +1,29 @@
+def make_data(student_id)
+  from random import seed
+  from random import random
+  seed(student_id)
+  import numpy as np
+  samples = 24*60*7
+  height = 500;
+  step = 10
+  n = samples*(height/step)
+  n = np.int(n);
+  number = np.zeros((n,1))
+  data = np.zeros((samples,np.int(height/step)))
+  r = (np.linspace(0.0001,1,50)**0.143)
+  s = np.linspace(0.1,1,50)
+  base = 25;
+  for i in range(0,np.int(height/step)):
+    a = np.zeros((samples,40))
+    for j in range(0,40):
+      tmp = random()
+      if tmp < 0.2: 
+        tmp = 1;
+      add = np.int((200-base) + base*tmp);
+      add = np.int(base*tmp)
+      # print(add)
+      a[:,j] =  np.sin(np.resize(np.linspace(0,2*np.pi,add),samples));
+    base = base + 5 
+    x = np.sum(a[:,:],1) ;
+    data[:,i] = (5*r[i] + x*s[i]);
+
